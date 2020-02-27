0e05ae0436874fd4eadeb0b756718a1b0d5ecde9
yourUsername <yourEmail@domain.com>
2020-02-27 17:19:16 -0500

    yy

---
+++ src/Cool.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Cool {
+
+public static void main(String[]args) {
+
+  // dog is in the container but if we put null that means there is nothing in the container
+
+    System.out.println( "enter a number:" );
+    String a = "homosexual";
+   System.out.println( a.length() );
+
+    String andrew = "Hello";
+    Scanner can = new Scanner (System.in);
+         System.out.println( "Enter the next number" );
+    // to get the next integer
+    System.out.println(can.nextInt()) ;
+
+
+
+
+
+
+
+
+
+}
+
+}
